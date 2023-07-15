76fa8d6400d0902837fb731b0a4252131fff640f
Mehadi14 <83648005+Mehadi14@users.noreply.github.com>
2023-07-15 22:48:34 +0530

    Add files via upload

---
+++ SpringBoot_hospitalApp/src/main/java/spring_boot_Hospitalapp/SpringBoot_hospitalApp/SpringBootHospitalAppApplication.java
@@ -0,0 +1,13 @@
+package spring_boot_Hospitalapp.SpringBoot_hospitalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHospitalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHospitalAppApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_hospitalApp/src/test/java/spring_boot_Hospitalapp/SpringBoot_hospitalApp/SpringBootHospitalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot_Hospitalapp.SpringBoot_hospitalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHospitalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
